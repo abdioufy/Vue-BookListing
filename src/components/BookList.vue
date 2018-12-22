5c95060947f82f299293583078be41ac235ef657
Abdou Diouf <abdou.diouf@soprasteria.com>
2018-12-22 10:52:03 +0100

    adding BookList file

---
+++ src/components/BookList.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+  name: "BookList",
+
+  data() {
+    return {
+      title: "All books"
+    };
+  }
+};
+</script>
+
+<style>
+h1,h2 {
+  font-weight: normal;
+}
+</style>
